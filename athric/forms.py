bec2a93c18e8a67a1133d597a386f28494fd509d
Obed101 <obedamoako92@gmail.com>
2022-09-08 12:09:26 +0000

    deploying athric

---
+++ athric/forms.py
@@ -0,0 +1,57 @@
+from wtforms import StringField, SubmitField, PasswordField, RadioField, SearchField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired as required, Length, EqualTo
+from flask_ckeditor import CKEditorField
+from flask_wtf.file import FileField, FileRequired
+
+
+class UserForm(FlaskForm):
+    """A form for taking user details for login, signup, etc."""
+    firstname = StringField('Your first name', [required()])
+    lastname = StringField('Your last name', [required()])
+    email = StringField('Your email', [required()])
+    role = StringField('Your current role')
+    tel = StringField('Your phone number', [Length(10, 12, 'Number is invalid')])
+    password = PasswordField('Your password')
+    confirm = PasswordField('Type your password again', [EqualTo(password)])
+    submit = SubmitField('Login')
+
+class Contact(FlaskForm):
+    """Creates and validates message"""
+    email = SearchField('Your Email Address', [required()])
+    name = StringField()
+    subject = StringField()
+    message = CKEditorField('Type in your message', [required()])
+    submit = SubmitField('Submit Message')
+
+
+class NewArticle(FlaskForm):
+    """Creates a post form"""
+    subject = StringField('Enter your post title')
+    message = CKEditorField()
+    category = StringField(validators=[required()])
+    submit = SubmitField()
+    cover = FileField('Cover image')
+
+
+class UploadFile(FlaskForm):
+    """File Upload Form"""
+    category = RadioField('Select the file type', [required()],
+    choices=[('images', 'Image'), ('documents', 'Document (Example: pdf, docx, etc)'), ('videos', 'Video')])
+    content = FileField('Cover image', [FileRequired('You must select a file')],)
+    f_name = StringField('Type in a short file name')
+    message = CKEditorField('Describe the file in detail (For videos and images)')
+    submit = SubmitField()
+
+
+class Search(FlaskForm):
+    search = SearchField(validators=[required(message='_')])
+    submit = SubmitField()
+
+class NoticeForm(FlaskForm):
+    """Creates a form for Notice"""
+    message = CKEditorField('Your notice body here', [
+                            required('Please type your notice')])
+    subject = StringField('Your notice title', [
+                          required('Please type your notice title')])
+    submit = SubmitField()
---
+++ athric/utils.py
@@ -0,0 +1,66 @@
+from flask import Flask
+from flask_ckeditor import CKEditor
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user
+from flask_msearch import Search
+from werkzeug.security import check_password_hash
+import os
+from flask_mail import Mail
+
+
+# u = os.environ.setdefault('ANSAH_GMAIL', 'ericansah219@gmail.com')
+# u = os.environ.setdefault('gmail_password', 'lbwwckhqmvhginkz')
+app = Flask(__name__)
+
+app.config['CKEDITOR_PKG_TYPE'] = 'basic'
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'If you\'re able to guess this key, then...'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+## Mail configurations ##
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+# app.config['TESTING'] = True
+app.config['SECURITY_EMAIL_SENDER'] = os.getenv('ansah_gmail')
+app.config['MAIL_USERNAME'] = os.getenv('ansah_gmail')
+app.config['MAIL_PASSWORD'] = os.getenv('gmail_password')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+db = SQLAlchemy(app)
+ckeditor = CKEditor(app)
+search = Search()
+search.init_app(app)
+mail = Mail(app)
+
+
+
+def change_in_db(old, new):
+    """
+    Modifies old data in database effectively
+    if only there has been a change  
+    Handles passwords differently
+    """
+    if old == current_user.password:
+        """Handling password differently"""
+        old = current_user.set_password(new) if new and not check_password_hash(old, new) else old
+    else:
+        old = new if not new == old else old
+        current_user.fullname = current_user.set_fullname()
+    db.session.commit()
+    return old
+
+admins = ['manager', 'superviser']
+at_categories = ['general_information', 'project_report', 'other_information']
+
+homes = [
+    {'name': 'Athric',
+        'description': 'Leading Ghana\'s Agriculture (Entomology/crops)',
+        'bg': '/static/images/1.jpg'},
+    {'name': 'Since 2008',
+        'description': 'Providing Domestic And Industrial Satisfaction',
+        'bg': '/static/images/3.jpg'},
+    {'name': 'Award Winning Team',
+        'description': 'Won 8 Awards Since 2010',
+        'bg': '/static/images/2.jpg'},
+]
